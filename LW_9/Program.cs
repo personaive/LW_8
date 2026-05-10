a2b7545bec3230bb4a28dd433309f4a30fe54c14
personaive <nik.bachurin2704@mail.ru>
2026-05-10 18:21:24 +0500

    Update Program.cs

--- LW_9/Program.cs
+++ LW_9/Program.cs
@@ -1,14 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-class Program
+public class Program
 {
-    static void Main()
+    public static void Main()
     {
         string path = "photos.dat";
         List<Photo> photos = PhotoService.Load(path);
+        string choice = "";
 
-        while (true)
+        while (choice != "0")
         {
             Console.WriteLine("\n1. Просмотр");
             Console.WriteLine("2. Добавить");
@@ -17,12 +18,19 @@ class Program
             Console.WriteLine("0. Выход");
 
             Console.Write("Выбор: ");
-            string choice = Console.ReadLine();
+            choice = Console.ReadLine();
 
             if (choice == "1")
             {
-                for (int i = 0; i < photos.Count; i++)
-                    Console.WriteLine(photos[i]);
+                if (photos.Count == 0)
+                {
+                    Console.WriteLine("Нет фотографий.");
+                }
+                else
+                {
+                    for (int i = 0; i < photos.Count; i++)
+                        Console.WriteLine(photos[i]);
+                }
             }
             else if (choice == "2")
             {
@@ -36,17 +44,25 @@ class Program
 
                 PhotoService.Add(photos, p);
                 PhotoService.Save(path, photos);
+                Console.WriteLine("Фотография добавлена!");
             }
             else if (choice == "3")
             {
-                string name = InputHelper.ReadString("Удалить: ");
-
-                PhotoService.RemoveByName(photos, name);
-                PhotoService.Save(path, photos);
+                if (photos.Count == 0)
+                {
+                    Console.WriteLine("Нет фотографий для удаления.");
+                }
+                else
+                {
+                    string name = InputHelper.ReadString("Удалить: ");
+                    PhotoService.RemoveByName(photos, name);
+                    PhotoService.Save(path, photos);
+                    Console.WriteLine("Фотография удалена (если существовала).");
+                }
             }
             else if (choice == "4")
             {
-                Console.WriteLine("1. По автору");
+                Console.WriteLine("\n1. По автору");
                 Console.WriteLine("2. Широкие фото");
                 Console.WriteLine("3. Кол-во");
                 Console.WriteLine("4. Последнее");
@@ -58,31 +74,56 @@ class Program
                     string author = InputHelper.ReadString("Автор: ");
                     var res = PhotoService.GetByAuthor(photos, author);
 
-                    for (int i = 0; i < res.Count; i++)
-                        Console.WriteLine(res[i]);
+                    if (res.Count == 0)
+                    {
+                        Console.WriteLine("Фотографий не найдено.");
+                    }
+                    else
+                    {
+                        for (int i = 0; i < res.Count; i++)
+                            Console.WriteLine(res[i]);
+                    }
                 }
                 else if (q == "2")
                 {
                     int w = InputHelper.ReadInt("Мин ширина: ");
                     var res = PhotoService.GetLargePhotos(photos, w);
 
-                    for (int i = 0; i < res.Count; i++)
-                        Console.WriteLine(res[i]);
+                    if (res.Count == 0)
+                    {
+                        Console.WriteLine("Фотографий не найдено.");
+                    }
+                    else
+                    {
+                        for (int i = 0; i < res.Count; i++)
+                            Console.WriteLine(res[i]);
+                    }
                 }
                 else if (q == "3")
                 {
                     string author = InputHelper.ReadString("Автор: ");
-                    Console.WriteLine(PhotoService.CountByAuthor(photos, author));
+                    int count = PhotoService.CountByAuthor(photos, author);
+                    Console.WriteLine($"Фотографий автора '{author}': {count}");
                 }
                 else if (q == "4")
                 {
-                    Console.WriteLine(PhotoService.GetLatestPhoto(photos));
+                    Photo latest = PhotoService.GetLatestPhoto(photos);
+                    if (latest != null)
+                        Console.WriteLine($"Последнее фото: {latest}");
+                    else
+                        Console.WriteLine("Фотографий нет");
+                }
+                else
+                {
+                    Console.WriteLine("Неверный выбор запроса.");
                 }
             }
-            else if (choice == "0")
+            else if (choice != "0")
             {
-                break;
+                Console.WriteLine("Неверный выбор. Попробуйте снова.");
             }
         }
+
+        Console.WriteLine("До свидания!");
     }
 }
